3f57959b9965f1c5bc36052ed0024ad7b27a4f66
Ramesh <ramgr498@gmail.com>
2024-04-23 11:09:12 +0530

    expense-mysql

--- frontend.sh
+++ frontend.sh
@@ -49,9 +49,9 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Rstarting nginx"
+VALIDATE $? "Restarting nginx"
 
